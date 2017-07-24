2df19e399cf34a24b8db674a2971f67a5b16b8d7
Marc L <marcll@vt.edu>
2017-07-24 11:35:55 -0400

    put together minimal B200 example
    had to change stream to A:A and set a gain higher than 0

---
+++ test_b200.py
@@ -0,0 +1,12 @@
+import pysdruhd as uhd
+import matplotlib.pyplot as plt
+import numpy as np
+
+center_freq = 100e6
+samp_rate = 4e6
+
+usrp = uhd.Usrp(type="b200", streams={"A:A": {'frequency':center_freq, 'gain':70}}, rate=samp_rate, gain=30.0)
+usrp.send_stream_command({'now': True})
+samples, metadata = usrp.recv()
+plt.psd(samples[0], NFFT=512, Fs=samp_rate/1e6, Fc=center_freq/1e6)
+plt.show()
